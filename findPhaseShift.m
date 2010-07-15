da5694f6f8c9c7d5fed8b8af748a1310f2ba39c7
Andrew Leifer <leifer@fas.harvard.edu>
2010-07-14 20:00:57 -0400

    Marc made some major improvements to the velocity finding algorithm.
    Gaussian low pass filter on the curvdata itself (spatially, not temporally)
    Accumulated averaging (exponential decay) with one weight, alpha at .85
    Instead of cropping 10% from head tail, we are inducing an assymetry and cropping 25% of head and 5% of tail.
    Smoothing the final phase velocity with lowpass1d.
    
    Things to do tomorrow. Go through and comment. Have an options structure. Also flag which of marc's functions are required and pull those.

--- findPhaseShift.m
+++ findPhaseShift.m
@@ -5,26 +5,39 @@ function ps = findPhaseShift(curvdata)
 % Modified by Andrew Leifer
 
 
-
+debug = false;
+sigma = 5;
 %pre-allucate the phase shift vector
 ps = zeros([1 (size(curvdata,1)-1)]);
 
-fracCrop=.1;
+headCrop=.2;
+tailCrop=0.05;
 
 %pre allocate x shift
 xs = 1:size(curvdata,2);
 
 %crop index
-cinds = xs (floor(length(xs)*fracCrop):ceil(length(xs)*(1-fracCrop)));
+cinds = xs (floor(length(xs)*headCrop):ceil(length(xs)*(1-tailCrop)));
 
 %This is a function that just shifts xdata some amount x 
 shiftfn = @(x,xdata) interp1(xs, xdata, cinds + x,'linear');
 x = 0;
 op = optimset('lsqcurvefit');
 op.Display = 'off';
+curvaccumulated = lowpass1d(curvdata(1,:),sigma);
+alpha_accum = 0.85;
 for j = 1:length(ps)
-   x = lsqcurvefit(shiftfn, x, curvdata(j,:), curvdata(j+1,cinds),-length(xs)*(1-fracCrop), length(xs)*(1-fracCrop), op);
-   ps(j) = x;
+   %find the phase shift by doing a least squares fit
+   tofitdata = lowpass1d(curvdata(j+1,:),sigma);
+   x = lsqcurvefit(shiftfn, x, curvaccumulated, tofitdata(:,cinds),-length(xs)*headCrop, length(xs)*tailCrop, op);
+   curvaccumulated = alpha_accum * interp1(xs,curvaccumulated,xs+x,'linear','extrap') + (1-alpha_accum) * tofitdata;
+   ps(j) = x; %record the phase shift
+   if (debug)
+       plot (xs, curvaccumulated, xs, tofitdata); ylim([min(curvdata(:)), max(curvdata(:))]);
+       legend ('average', 'current');
+       title (num2str(j));
+       pause(0.05);
+   end
 end
 
 %Sum the individual phase shifts to get a cumulitive phase shift
